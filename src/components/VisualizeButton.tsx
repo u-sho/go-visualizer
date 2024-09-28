d64499ae57ebdb6a6d66ada09e5f956983a21e88
kaiyasuda1204 <kaiyasuda1204@gmail.com>
2024-09-28 16:56:19 +0900

    Visualボタン作った

--- src/components/VisualizeButton.tsx
+++ src/components/VisualizeButton.tsx
@@ -1,3 +1,27 @@
+// src/components/VisualButton.tsx
+"use client";
+
+import React, { useState } from 'react';
+
 export const VisualButton = () => {
-    return <button>可視化</button>
-}
+  const [color, setColor] = useState('black');
+
+  const toggleColor = () => {
+    setColor(prevColor => (prevColor === 'black' ? 'white' : 'black'));
+  };
+
+  return (
+    <button
+      className={`px-4 py-2 rounded-md border-2 cursor-pointer text-lg ${
+        color === 'black' 
+          ? 'bg-black text-white border-gray-700'
+          : 'bg-white text-black border-gray-700'
+      }`}
+      onClick={toggleColor}
+    >
+      {color === 'black' ? 'Visualize On' : 'Visualize Off'}
+    </button>
+  );
+};
+
+
