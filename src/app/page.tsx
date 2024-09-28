7e65f3d853340aeb88c523ad355e0343f5417173
kaiyasuda1204 <kaiyasuda1204@gmail.com>
2024-09-28 18:35:43 +0900

    ボタン配置が完成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,19 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <DeleteButton></DeleteButton>
-      <Gameboard></Gameboard>
-      <StoneButton></StoneButton>
-      <VisualButton></VisualButton>
+    <div className="flex flex-col sm:flex-row items-center justify-between min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      
+      {/* Gameboardを画面の左側に固定 */}
+      <div className="sm:fixed sm:left-8 sm:top-1/2 sm:transform sm:-translate-y-1/2">
+        <Gameboard />
+      </div>
+      
+      {/* ボタン群を右に、または小さい画面では下に表示 */}
+      <div className="flex flex-col sm:flex-col gap-4 sm:ml-auto sm:flex-row sm:items-center">
+        <VisualButton />
+        <StoneButton />
+        <DeleteButton />
+      </div>
     </div>
   );
 }
