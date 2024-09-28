c4fa6931270820e8b8b02b14f1419e2790c5c892
kaiyasuda1204 <kaiyasuda1204@gmail.com>
2024-09-28 18:40:08 +0900

    "消す"を"Delete"に変更

--- src/components/DeleteButton.tsx
+++ src/components/DeleteButton.tsx
@@ -1,5 +1,5 @@
 export const DeleteButton = () => {
     return <button style={{ backgroundColor: 'red', color: 'white', padding: '10px 20px', borderRadius: '5px', border: 'none', cursor: 'pointer' }}>
-    消す
+    Delete
   </button>
 }
