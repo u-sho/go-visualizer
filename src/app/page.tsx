3e98af5e56622ca8c17cd6cb0a2fe81095c4d16c
u-sho <shouhei.uechi@gmail.com>
2024-09-28 17:42:44 +0900

    (wip) add game board

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,20 @@
 import { DeleteButton } from "@/components/DeleteButton";
-import { Gameboard } from "@/components/GameBoard";
+import { GameBoard } from "@/components/GameBoard";
 import { GameStone } from "@/components/GameStone";
 import { VisualButton } from "@/components/VisualizeButton";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <DeleteButton></DeleteButton>
-      <Gameboard></Gameboard>
+      <GameBoard
+        canvasWidth={300}
+        canvasHeight={300}
+        axisX={0}
+        axisY={0}
+        rectWidth={300}
+        rectHeight={300}
+        size={19} />
       <GameStone></GameStone>
       <VisualButton></VisualButton>
     </div>
--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -1,3 +1,106 @@
-export const Gameboard = () => {
-    return <button>盤</button>
-}
+"use client";
+
+import { useCallback, useEffect, useRef } from "react";
+
+type CanvasProps = {
+  canvasWidth: number;
+  canvasHeight: number;
+  axisX: number;
+  axisY: number;
+  rectWidth: number;
+  rectHeight: number;
+  size: number;
+};
+
+export const GameBoard = (props: CanvasProps) => {
+  const {
+    canvasWidth,
+    canvasHeight,
+    axisX,
+    axisY,
+    rectWidth,
+    rectHeight,
+    size,
+  } = props;
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  const startAxis = 30;
+  const endAxis = 270;
+  const distance = 40;
+
+  const getCtx = (): CanvasRenderingContext2D => {
+    const canvas: HTMLCanvasElement | null = canvasRef.current;
+    if (!canvas?.getContext) throw new Error("Something may happen.");
+    return canvas.getContext("2d") as CanvasRenderingContext2D;
+  };
+
+  type Context2DCallbackOptions = {
+    fgColor: string;
+    bgColor: string;
+    startX: number;
+    startY: number;
+  }&({
+    endX: number;
+    endY: number;
+  }|{
+    width: number;
+    height: number;
+  });
+
+  const drawRect = useCallback(
+    (ctx: CanvasRenderingContext2D, {bgColor='white'}: Pick<Partial<Context2DCallbackOptions>, 'bgColor'>) => {
+      const rect = new Path2D();
+      rect.rect(axisX, axisY, rectWidth, rectHeight);
+      ctx.beginPath();
+      ctx.fillStyle = bgColor;
+      ctx.fill(rect);
+    },
+    [axisX, axisY, rectWidth, rectHeight]
+  );
+
+  const drawLine = useCallback(
+    (ctx: CanvasRenderingContext2D, {fgColor='black'}: Pick<Partial<Context2DCallbackOptions>, 'fgColor'>) => {
+      ctx.beginPath();
+      ctx.strokeStyle = fgColor;
+      ctx.lineWidth = 2;
+      for (let i = 0; i < size; i++) {
+        const y = startAxis + i * distance;
+        const x = startAxis + i * distance;
+        ctx.moveTo(startAxis, y);
+        ctx.lineTo(endAxis, y);
+        ctx.moveTo(x, startAxis);
+        ctx.lineTo(x, endAxis);
+      }
+      ctx.stroke();
+    },
+    [size]
+  );
+
+  const drawArc = useCallback(
+    (ctx: CanvasRenderingContext2D, {fgColor='black'}: Pick<Partial<Context2DCallbackOptions>, 'fgColor'>) => {
+      const radius = 5;
+      const startAngle = 0;
+      const endAngle = Math.PI * 2;
+      ctx.beginPath();
+      ctx.fillStyle = fgColor;
+      for (let i = 0; i < 3; i++) {
+        const pointAxis = startAxis + distance * i;
+        ctx.moveTo(pointAxis, pointAxis);
+        ctx.arc(pointAxis, pointAxis, radius, startAngle, endAngle, true);
+      }
+      ctx.fill();
+    },
+    []
+  );
+
+  const drawAll = useCallback(() => {
+    const ctx: CanvasRenderingContext2D = getCtx();
+    drawRect(ctx,{});
+    drawLine(ctx,{});
+    drawArc(ctx, {fgColor: "red"});
+  }, [drawRect, drawLine, drawArc]);
+
+  useEffect(() => drawAll(), [drawAll]);
+
+  return <canvas ref={canvasRef} width={canvasWidth} height={canvasHeight} />;
+};
